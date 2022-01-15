747409f24ff6851c8f4fb332b8c15237bdeae715
Ilodi2k <marcel.strohm@web.de>
2022-01-15 23:22:06 +0100

    added UI (using DOM-manipulation)

--- script.js
+++ script.js
@@ -1,62 +1,65 @@
-function computerPlay() {
-    const choices = ["rock", "paper", "scissors"];
-    return choices[Math.floor(Math.random() * choices.length)];
-}
-
-let playerScore = 0;
+const buttons = document.querySelectorAll('.choices');
+const displayPlayerScore = document.querySelector('#user-score');
+const displayComputerScore = document.querySelector('#computer-score');
+const scoreMessage = document.querySelector('#score-message');
+const restartButton = document.querySelector('#restart');
 let computerScore = 0;
+let playerScore = 0;
 
-function playerInput() {
-    choice = prompt("Choose: Rock Paper or Scissors!");
-    if (choice.toLowerCase() == "rock" || choice.toLowerCase() == "paper" || choice.toLowerCase() == "scissors") {
-        return choice.toLowerCase();
-    } else {
-        alert("Look like you had a typo, try again. Either Rock, Paper or Scissors");
-        return playerInput();
-    }
+//Play the Game
+function playGame(e) {
+    const playerChoice = e.target.id;
+    const computerChoosing = buttons[Math.floor(Math.random() * buttons.length)];
+    const computerChoice = computerChoosing.id;
+    playRound(computerChoice, playerChoice);
+    updateScore(computerScore, playerScore);
 }
 
+// Event Listerners
+buttons.forEach((button) => button.addEventListener('click', playGame));
+restartButton.addEventListener('click', restartGame);
+
+// play a round (determine winner)
 function playRound(computerChoice, playerChoice) {
-     if (computerChoice == "paper" && playerChoice == "rock") {
-        console.log("You lose! Paper beats Rock");
-        return computerScore++;
-    } else if (computerChoice == "paper" && playerChoice == "scissors") {
-        console.log("You win! Scissors beats Paper");
-        return playerScore++;
-    } else if (computerChoice == "rock" && playerChoice == "paper") {
-        console.log("You win! Paper beats Rock");
-        return playerScore++;
-    } else if (computerChoice == "rock" && playerChoice == "scissors") {
-        console.log("You lose! Rock beats Scissors");
-        return computerScore++;
-    } else if (computerChoice == "scissors" && playerChoice == "paper") {
-        console.log("You lose! scissors beats Paper");
-        return computerScore++;
-    } else if (computerChoice == "scissors" && playerChoice == "rock") {
-        console.log("You win! Rock beats Scissors");
-        return playerScore++;
-    } else if (computerChoice == playerChoice) {
-        console.log("It's a draw! Both of you picked " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1));
-    } else {
-        console.log("iwo ist ein Fehler");
-    }
+    if (computerChoice == "paper" && playerChoice == "rock") {
+        scoreMessage.textContent = "You lose! Paper beats Rock";
+    //    console.log("You lose! Paper beats Rock");
+       return computerScore++;
+   } else if (computerChoice == "paper" && playerChoice == "scissors") {
+        scoreMessage.textContent = "You win! Scissors beats Paper";
+    //    console.log("You win! Scissors beats Paper");
+       return playerScore++;
+   } else if (computerChoice == "rock" && playerChoice == "paper") {
+        scoreMessage.textContent = "You win! Paper beats Rock";
+    //    console.log("You win! Paper beats Rock");
+       return playerScore++;
+   } else if (computerChoice == "rock" && playerChoice == "scissors") {
+        scoreMessage.textContent = "You lose! Rock beats Scissors";
+    //    console.log("You lose! Rock beats Scissors");
+       return computerScore++;
+   } else if (computerChoice == "scissors" && playerChoice == "paper") {
+        scoreMessage.textContent = "You lose! Scissors beats Paper";
+    //    console.log("You lose! scissors beats Paper");
+       return computerScore++;
+   } else if (computerChoice == "scissors" && playerChoice == "rock") {
+        scoreMessage.textContent = "You win! Rock beats Scissors";
+    //    console.log("You win! Rock beats Scissors");
+       return playerScore++;
+   } else if (computerChoice === playerChoice) {
+        scoreMessage.textContent = "It's a draw! Both of you picked " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+    //    console.log("It's a draw! Both of you picked " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1));
+   } else {
+       console.log("ERROR: There seems to be a mistake somewhere!!");
+   }
 }
 
-function game(computerChoice, playerChoice) {
-    console.log("\n \n \n \n \n \n \n \n \n \n \n \n \n");
-    for (let i=0; i<5; i++) {
-        let computerChoice = computerPlay();
-        let playerChoice = playerInput();
-        playRound(computerChoice, playerChoice);
-        console.log("Score: Computer " + computerScore + "   ---   You: " + playerScore);
-    }
-    if (computerScore > playerScore){
-        console.log("The computer has won this round. Press F5 to try again.")
-    } else if (playerScore > computerScore){
-        console.log("You won this Round! Press F5 to restart.")
-    } else {
-        console.log("It's a tie! Press F5 to try again.");
-    }
+//update score
+function updateScore (computerScore, playerScore) {
+    displayComputerScore.textContent = computerScore;
+    displayPlayerScore.textContent = playerScore;
 }
 
-game();
+//restart the Game
+function restartGame() {
+location.reload();
+}
